b63b64632884a0ef0674d1462e374c95aadf432b
Brice Leahy <sickair45@gmail.com>
2023-03-05 12:04:28 -0800

    refactor solution

--- 6kyu-multiples-of-3-and-5/app.js
+++ 6kyu-multiples-of-3-and-5/app.js
@@ -8,27 +8,37 @@
 //R:return
 //E:20 input (3,5,6,9,10,12,15,18) return 
 //P:
-function multiplesOf(num){
- let sum = []
-    //check for neg num
- if(num >= -1){
-    //count from 3 to number
-    for(let i = 3; i < num; i++){
-        //if multiple of 3 and 5 display num
-        if(i % 3 === 0 && i % 5 === 0){
-          sum.push(i)
-        //if multiple of 3 display num  
-        } else if(i % 3 === 0){
-          sum.push(i)
-        //if multiple of 5 display num  
-        } else if(i % 5 === 0){
-          sum.push(i)
-        } 
+// function multiplesOf(num){
+//  let sum = []
+//     //check for neg num
+//  if(num >= -1){
+//     //count from 3 to number
+//     for(let i = 3; i < num; i++){
+//         //if multiple of 3 and 5 display num
+//         if(i % 3 === 0 && i % 5 === 0){
+//           sum.push(i)
+//         //if multiple of 3 display num  
+//         } else if(i % 3 === 0){
+//           sum.push(i)
+//         //if multiple of 5 display num  
+//         } else if(i % 5 === 0){
+//           sum.push(i)
+//         } 
+//     }
+//  } else {
+//     sum.push(0)
+//  }
+//  return sum.reduce((acc, el) => acc + el,0)
+// }
+//refactor
+function multiplesOf(number){
+    let sum = 0
+    for(let i = 3; i < number;i++){
+        if(i % 3 == 0 || i % 5 == 0){
+            sum += i
+        }
     }
- } else {
-    sum.push(0)
- }
- return sum.reduce((acc, el) => acc + el,0)
-}
-multiplesOf(10) //20 (3,5,6,9,10,12,15,18)
+    return sum
+  }
+multiplesOf(10) //23 (3,5,6,9,)
 multiplesOf(-3) // (0)
